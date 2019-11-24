2d461a271d0ce2aaeb646d43d3bafff63859a5d3
Ronaldo jose <ronaldo.jrk@gmail.com>
2019-11-24 14:40:15 -0300

    decimoteste

--- src/routes.js
+++ src/routes.js
@@ -1,15 +1,23 @@
 const express = require('express');
 const multer = require('multer');
 const uploadConfig = require('./config/upload');
+const routes = express.Router();
+const upload = multer(uploadConfig);
+
+
+
 
 const SessionController =require('./controllers/SessionController');
 const LivroController =require('./controllers/LivroController');
 const LoginController =require('./controllers/LoginController');
 const PlaylivroController =require('./controllers/PlaylivroController');
 
-const routes = express.Router();
-const upload = multer(uploadConfig);
-routes.get('/',SessionController.index);
+
+
+
+routes.get('/',(req,res)=>{
+  return res.json({message:"hello"});
+});
 routes.get('/sessions',SessionController.index);
 routes.post('/sessions',SessionController.store);
 
